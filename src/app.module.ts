c8490d763834207be8746c127c9fbba86dc22fe2
cloudwizardx <cloudwizardx@gmail.com>
2025-07-09 23:17:27 +0700

    feat: create chatting schema extends Document

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PrismaModule } from './prisma/prisma.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), 
     MongooseModule.forRoot(process.env.DATABASE_URL || ""),
-    PrismaModule]
+    PrismaModule,
+    ChatModule]
 })
 export class AppModule { }
---
+++ src/chat/chat.schema.ts
@@ -0,0 +1,70 @@
+import { Prop, Schema } from "@nestjs/mongoose"
+
+@Schema()
+export class Attachment {
+    @Prop()
+    url: string
+
+    @Prop()
+    fileName: string
+
+    @Prop()
+    type: string
+}
+
+@Schema({ timestamps: true })
+export class Message extends Document {
+    @Prop({required: true})
+    senderId: string
+
+    @Prop({required: true})
+    receiverId: string
+
+    @Prop({required: true, enum: ['Doctor', 'User', 'Manager', 'Admin']})
+    senderRole: string
+
+    @Prop({required: true})
+    content: string
+
+    @Prop({default: Date.now})
+    timestamp: Date
+
+    @Prop({required: true})
+    conversationId: string
+
+    @Prop({default: 'Sent', enum: ['Sent', 'Delivered', 'Seen']})
+    status: 'Sent' | 'Delivered' | 'Seen'
+
+    @Prop([Attachment])
+    attachments: Attachment[]
+}
+
+@Schema({timestamps: true})
+export class Conversation extends Document {
+    @Prop({required: true, unique: true})
+    conversationId: string
+
+    @Prop({required: true})
+    userId1: string 
+
+    @Prop({required: true})
+    userId2: string
+
+    @Prop()
+    lastMessage: string 
+
+    @Prop({default: Date.now})
+    lastMessageTime: Date
+
+    @Prop({type: {
+        user1: {type: Number, default: 0},
+        user2: {type: Number, default: 0}
+    }})
+    unreadCount: {
+        user1: number,
+        user2: number
+    }
+}
+
+
+
