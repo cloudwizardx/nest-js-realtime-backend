4aa2932887c78570d43e1e1ca1b4ad6d4c53cd11
cloudwizardx <cloudwizardx@gmail.com>
2025-07-10 21:13:41 +0700

    feat: implements chat socket gateway with  handleConnectionSocket

---
+++ src/chat/gateway/chat.gateway.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { UseGuards } from "@nestjs/common";
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
+import { WsAuthGuard } from "src/common";
+import { ChatService } from "../chat.service";
+
+
+@WebSocketGateway({
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST',],
+        credentials: true
+    },
+    namespace: '/chat'
+})
+@UseGuards(WsAuthGuard)
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    @WebSocketServer()
+    server: Server
+
+    private connectedUsers = new Map<string, string>()
+
+    constructor(private chatService: ChatService) { }
+
+    async handleDisconnect(client: Socket) {
+
+    }
+
+    async handleConnection(client: Socket) {
+        try {
+            const user = client.data.user
+            this.connectedUsers.set(user.userId, client.id)
+            await this.chatService.updateUserOnlineStatus(user.userId, true)
+            client.join(`user_${user.userId}`)
+
+            this.server.emit('userOnline', { userId: user.userId, isOnline: true })
+            console.log(`User ${user.userId} connected`)
+        } catch (error) {
+            console.log(error)
+            client.disconnect()
+        }
+    }
+
+}
