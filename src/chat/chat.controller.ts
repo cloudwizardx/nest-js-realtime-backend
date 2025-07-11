2e4d61ae118eec1dea7756ec8a43039a6d8d500d
cloudwizardx <cloudwizardx@gmail.com>
2025-07-11 09:42:21 +0700

    feat: implement [getConversation] getConversation chat/conversation @CurrentUser

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,6 +1,24 @@
-import { Controller, UseGuards } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../common/guard';
+import { ChatService } from './chat.service';
+import { CurrentUser } from 'src/common';
 
 @Controller('chat')
 @UseGuards(JwtAuthGuard)
-export class ChatController {}
+export class ChatController {
+
+    constructor(private chatService: ChatService) { }
+
+    @Get('conversations')
+    async getConversations(@CurrentUser() user: any) {
+        return this.chatService.getConversations(user.userId)
+    }
+
+    @Get('specific-conversation')
+    async geet
+
+
+
+}
