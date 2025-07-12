9c64d090f4ad9c6d97c7aaedcedfc9d18dc6fbec
cloudwizardx <cloudwizardx@gmail.com>
2025-07-12 10:06:54 +0700

    feat: implement [get] getMessages chat/messages/:conversationId

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../common/guard';
 import { ChatService } from './chat.service';
 import { CurrentUser } from 'src/common';
@@ -21,6 +21,13 @@ export class ChatController {
         return this.chatService.getSpecificConversation(user.userId, userId2)
     }
 
-
+    @Get('messages/:conversationId')
+    async getMessages(
+        @Param('conversationId') conversationId: string,
+        @Query('page') page: number = 1,
+        @Query('limit') limit: number = 50,
+    ) {
+        return this.chatService.getMessages({conversationId, page, limit});
+    }
 
 }
